058d8ccee6d6965257d68729ee4b767b59d6e451
LeXini <marcos_biazus@hotmail.com>
2025-12-14 17:17:46 -0300

    Criação do teste de remoção de usuário

--- tests/api/api.spec.ts
+++ tests/api/api.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('cadastro', () => {
-  test('deve poder cadastrar um novo usuário', async ({ request }) => {
+test.describe('Cadastro De Usuário', () => {
+  test('Deve poder cadastrar um novo usuário', async ({ request }) => {
     const response = await request.post('/usuarios',{
       data: {
         "nome": "Gremio",
@@ -14,7 +14,27 @@ test.describe('cadastro', () => {
     expect(response.status()).toBe(201);
 
     const body = await response.json();
-
     expect(body.message).toBe('Cadastro realizado com sucesso')
+  })
+
+  test.only('Deve poder remover um usuário', async ({ request }) => {
+    const cadastro = await request.post('/usuarios',{
+      data: {
+        "nome": "Gremio",
+        "email": "gremio@teste.com",
+        "password": "gremio123",
+        "administrador": "true"
+      }
+    });
+    
+    const bodyCadastro = await cadastro.json();
+    const usuario = bodyCadastro._id
+
+    const deleteUsuario = await request.delete(`/usuarios/${usuario}`)
+    
+    expect(deleteUsuario.status()).toBe(200);
+
+    const body = await deleteUsuario.json();
+    expect(body.message).toBe('Registro excluído com sucesso')
   })
 })
